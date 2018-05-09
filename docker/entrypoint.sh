ed62e0ef5fbac72d1384511c66e91308238f26e9
Pete Florence <pete.florence@gmail.com>
2018-05-09 19:47:18 -0400

    add ros to dockerbuild, utility functions in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,22 @@ function use_pytorch_dense_correspondence()
 
 export -f use_pytorch_dense_correspondence
 
+function use_ros()
+{	
+	source /opt/ros/kinetic/setup.bash
+    source ~/catkin_ws/devel/setup.bash
+}
+
+export -f use_ros
+
+function use_all()
+{	
+	use_ros
+    use_pytorch_dense_correspondence
+}
+
+export -f use_all
+
 exec "$@"
 
 cd ~/code
