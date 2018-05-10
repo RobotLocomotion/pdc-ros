550d0574397be67ac2d608e5f48fc8c10576c357
Pete Florence <pete.florence@gmail.com>
2018-05-10 14:10:51 -0400

    this actually can return match_found = True

--- catkin_ws/src/pdc_ros/src/pdc_ros/pdc_ros_wrapper.py
+++ catkin_ws/src/pdc_ros/src/pdc_ros/pdc_ros_wrapper.py
@@ -9,14 +9,19 @@ import actionlib
 # pdc_ros_msgs
 import pdc_ros_msgs.msg
 
+import torch
+
 class PDCRos(object):
 
 	def __init__(self):
 		pass
 
 	def run(self):
+		print "new"
 		rospy.loginfo("staring PDCRos")
 		self._setup_ros_actions()
+		A = torch.rand(3)
+		print A
 		rospy.spin()
 
 	def _setup_ros_actions(self):
@@ -31,6 +36,9 @@ class PDCRos(object):
 
 	def _on_find_best_match_action(self, goal):
 		print "received FindBestMatchAction request"
-		return
+		result = pdc_ros_msgs.msg.FindBestMatchResult()
+		result.match_found = True
+		self._find_best_match_action_server.set_succeeded(result)
+
 
 
