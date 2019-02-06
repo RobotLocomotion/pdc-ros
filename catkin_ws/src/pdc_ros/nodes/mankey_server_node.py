a9c4f617a0c4b8182dfd13209cd594e86211f8ad
manuelli <lucas.t.manuelli@gmail.com>
2019-02-06 14:41:25 -0500

    minor updates

--- catkin_ws/src/pdc_ros/nodes/mankey_server_node.py
+++ catkin_ws/src/pdc_ros/nodes/mankey_server_node.py
@@ -4,8 +4,8 @@ import rospy
 from pdc_ros.mankey.mankey_ros_server import ManKeyROSServer
 
 if __name__ == "__main__":
-    rospy.init_node("mankey")
-    poser_ros = ManKeyROSServer.make_default()
-    poser_ros.run()
+    rospy.init_node("ManKey")
+    mankey_ros = ManKeyROSServer.make_default()
+    mankey_ros.run()
     rospy.loginfo("starting to spin")
     rospy.spin()
